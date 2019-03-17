a25b61189a2f52a43a2b915ee7c58244a986ce77
Michael Hillebrand <michaelhillebrand@ou.edu>
2019-03-17 15:05:47 -0700

    changes to discord bot

--- discord_bot.py
+++ discord_bot.py
@@ -13,22 +13,26 @@ class HuskieBot(discord.Client):
 
     def __init__(self, *, loop=None, **options):
         self.scraper = Scraper(self)
-        self.salt_channel = Channel(**{'id': '555186709772501013', 'server': Server(**{'id': '100708750096080896'})})
+        self.doc_bop = None
+        self.salt_channel = None
+        self.will = None
         super().__init__(loop=loop, **options)
 
     def send_to_salt(self, message):
         self.loop.create_task(self.send_message(self.salt_channel, message))
 
     async def on_ready(self):
+        await self.change_presence(game=discord.Game(name='Salty Bet'))
+        self.scraper = Scraper(self)
+        self.doc_bop = self.get_server('100708750096080896')
+        self.salt_channel = self.doc_bop.get_channel('555186709772501013')
+        self.will = self.doc_bop.get_member_named('ARDelta#9051')
         message = '////////////////////\n' \
-                  '/                  /\n' \
-                  '/                  /\n' \
-                  '/                  /\n' \
-                  '/     HuskieBot    /\n' \
-                  '/    Initialized   /\n' \
-                  '/                  /\n' \
-                  '/                  /\n' \
-                  '////////////////////\n\n' \
+                  '////////////////////\n' \
+                  '////**HuskieBot**////\n' \
+                  '///**Initialized**///\n' \
+                  '///////////////////\n' \
+                  '///////////////////\n\n' \
                   'For a list of available commands, type !commands'
         self.send_to_salt(message)
 
@@ -40,11 +44,14 @@ class HuskieBot(discord.Client):
 
         elif message.content.startswith('!commands'):
             await self.send_message(message.channel,
-                                    'status\t\t- Display status of HuskieBot\n'
-                                    'learn\t\t- Starts HuskieBot\'s learning algorithm '
-                                    '(This will disable most commands while it is running\n'
-                                    'stoplearn\t\t- Stops HUskieBot\'s learning\n'
-                                    'mute\t\t- Stops HuskieBot from posting stats about the matches'
+                                    "hello\t\t- Say hi to Huskie Bot\n"
+                                    "status\t\t- Display status of HuskieBot\n"
+                                    "learn\t\t- Starts HuskieBot\'s learning algorithm "
+                                    "(This will disable most commands while it is running\n"
+                                    "stoplearn\t\t- Stops Huskie Bot\'s learning\n"
+                                    "mute\t\t- Stops HuskieBot from posting stats about Salty Bet matches\n"
+                                    "unmute\t\t- Huskie Bot will post stats about Salty Bet matches\n"
+                                    "shutup\t\t- Huskie Bot will tell Will to shutup\n"
                                     )
 
         elif message.content.startswith('!status'):
@@ -83,3 +90,11 @@ class HuskieBot(discord.Client):
         elif message.content.startswith('!stopscrape'):
             self.scraper.stop()
             await self.send_message(message.channel, 'Stopped scraping')
+
+        elif message.content.startswith('!shutup'):
+            self.scraper.stop()
+            await self.send_message(message.channel, '{} Shut up!'.format(self.will.mention))
+
+    async def close(self):
+        await self.send_message(self.salt_channel, 'Shutting Down...')
+        return super().close()
--- run.py
+++ run.py
@@ -8,4 +8,4 @@ if __name__ == '__main__':
         client.run('NTU1NTEyNDY2OTM1OTA2MzA0.D2sqLA.8tSvTx9EFGw2IngO_y4byS49SQY')
         # client.run(sys.argv[1])
     except IndexError:
-        print('Key not provided - Unable to start Bot')
+        print('Key not provided - Unable to start bot')
