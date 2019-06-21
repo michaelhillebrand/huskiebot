5f3c4e2b843cc453507cc27bc6a591fadced62f6
Michael Hillebrand <michaelhillebrand@ou.edu>
2019-06-21 15:47:39 -0700

    removes discord key

--- run.py
+++ run.py
@@ -5,7 +5,6 @@ from discord_bot import HuskieBot
 if __name__ == '__main__':
     try:
         client = HuskieBot()
-        # client.run('NTU1NTEyNDY2OTM1OTA2MzA0.D2sqLA.8tSvTx9EFGw2IngO_y4byS49SQY')
         key = open(sys.argv[1])
         client.run(key.read())
     except IndexError:
