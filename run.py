9d48e58935e10e57a254c57f7bc1f0e01c245cf5
Michael Hillebrand <michaelhillebrand@ou.edu>
2019-06-21 16:38:23 -0700

    revert

--- run.py
+++ run.py
@@ -5,7 +5,8 @@ from discord_bot import HuskieBot
 if __name__ == '__main__':
     try:
         client = HuskieBot()
-        key = open(sys.argv[1])
-        client.run(key.read())
+        client.run('NTU1NTEyNDY2OTM1OTA2MzA0.D2sqLA.8tSvTx9EFGw2IngO_y4byS49SQY')
+        # key = open(sys.argv[1])
+        # client.run(key.read())
     except IndexError:
         print('Key not provided - Unable to start bot')
