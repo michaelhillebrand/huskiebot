23d5b82a8b3d4acf47e2bbd02b89df93f4424440
Will Poorman <will.k.poorman@gmail.com>
2019-06-25 21:38:32 -0500

    Switch to using env vars for discord token
    Using the dotenv package, we can load a base .env variable at the start 
    of the app to pull in the discord token from this file which won't be 
    stored in git. Also includes a .template.env file to define up how the 
    .env file should look

--- run.py
+++ run.py
@@ -1,12 +1,19 @@
-import sys
+# Standard library
+import os
 
+# Pip installed libraries
+import dotenv
+
+# Local libraries
 from discord_bot import HuskieBot
 
 if __name__ == '__main__':
+    # Config
+    dotenv.load_dotenv()
+    DISCORD_BOT_TOKEN = os.getenv('DISCORD_BOT_TOKEN')
+
     try:
         client = HuskieBot()
-        client.run('NTU1NTEyNDY2OTM1OTA2MzA0.D2sqLA.8tSvTx9EFGw2IngO_y4byS49SQY')
-        # key = open(sys.argv[1])
-        # client.run(key.read())
+        client.run(DISCORD_BOT_TOKEN)
     except IndexError:
         print('Key not provided - Unable to start bot')
