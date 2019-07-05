f0502419471c5e7181a8026299d1106071dd5464
mwilcoxen <XmarX360@gmail.com>
2019-07-05 15:52:14 -0500

    Makes pocket sand command

--- commands/pocket_sand.py
+++ commands/pocket_sand.py
@@ -1,11 +1,13 @@
+from PIL import Image
+
 from commands.base import BaseCommand
+from discord_bot import STATIC_PATH
 
 
 class PocketSand(BaseCommand):
     trigger = 'shutup'
     description = 'HuskieBot will tell Will to shutup'
-
-    will = None
+    flare_img = Image.open(STATIC_PATH + 'images/flare.png')
 
     async def command(self, message):
         """
