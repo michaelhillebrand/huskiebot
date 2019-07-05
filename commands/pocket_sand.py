9443f66ba264fdc1080fcd38765f9c6bdcb878b0
mwilcoxen <XmarX360@gmail.com>
2019-07-05 15:50:43 -0500

    Makes pocket sand command

---
+++ commands/pocket_sand.py
@@ -0,0 +1,26 @@
+from commands.base import BaseCommand
+
+
+class PocketSand(BaseCommand):
+    trigger = 'shutup'
+    description = 'HuskieBot will tell Will to shutup'
+
+    will = None
+
+    async def command(self, message):
+        """
+        HuskieBot tells Will to shutup
+
+        Parameters
+        ----------
+        message : discord.Message
+
+        Returns
+        -------
+        str
+            Will's User mention
+
+        """
+        if not self.will:
+            self.will = self.client.get_user(187364654455062528)
+        await message.channel.send('{} Shut up!'.format(self.will.mention))
