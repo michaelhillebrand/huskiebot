e9388eb1c489eb3730820f24eb8a64005e742748
Michael Hillebrand <michaelhillebrand@ou.edu>
2019-07-14 19:07:20 -0700

    restores magic number for purge command

--- cogs/chat_moderate.py
+++ cogs/chat_moderate.py
@@ -25,19 +25,18 @@ class ChatModerator(BaseCog, command_attrs={'hidden': True}):
         None
 
         """
+        min_purge_limit = 1
         if ctx.channel.type == discord.ChannelType.private:
             await ctx.send('I can\'t purge a DM Channel')
         elif ctx.author.guild_permissions.administrator:
             try:
-                if purge_limit < 1:
+                if purge_limit < min_purge_limit:
                     raise ValueError
                 logging.info("Purging the last {limit} message(s) "
                              "from channel: {channel}".format(limit=purge_limit, channel=ctx.channel.name))
                 await ctx.channel.purge(limit=purge_limit + 1)
             except ValueError:
                 await ctx.send('That is not a valid number')
-            except discord.errors.Forbidden:
-                await ctx.send('I do no have the permissions to do that')
         else:
             await ctx.send('You do no have the permissions to do that')
 
