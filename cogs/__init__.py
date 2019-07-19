a1e5578e0bff89a427900fe1c3bd79299f42aaac
Michael Hillebrand <michaelhillebrand@ou.edu>
2019-07-18 22:14:54 -0700

    adds deepfry command; updates requirements

--- cogs/__init__.py
+++ cogs/__init__.py
@@ -3,5 +3,6 @@ from os.path import abspath, join, dirname, exists
 
 BASE_PATH = dirname(dirname(abspath(__file__)))
 MEDIA_PATH = join(BASE_PATH, 'media/')
+STATIC_PATH = join(BASE_PATH, 'static/')
 if not exists(MEDIA_PATH):
     os.makedirs(MEDIA_PATH)
--- cogs/dank_memes.py
+++ cogs/dank_memes.py
@@ -1,17 +1,25 @@
 import logging
 import os
 import typing
+from collections import namedtuple
 from io import BytesIO
 from random import randint
+from tempfile import NamedTemporaryFile
 from zipfile import ZipFile
 
+import cv2
 import discord
-from PIL import Image
+import math
+import numpy
+import requests
+from PIL import Image, ImageOps, ImageEnhance
 from discord.ext import commands
 
-from cogs import MEDIA_PATH
+from cogs import MEDIA_PATH, STATIC_PATH
 from cogs.base import BaseCog
 from utils.download import download
+
+FlarePosition = namedtuple('FlarePosition', ['x', 'y', 'size'])
 
 
 class DankMemes(BaseCog):
@@ -62,6 +70,91 @@ class DankMemes(BaseCog):
             await self._process_image(BytesIO(zip_file.read(file.filename)))
         zip_file.close()
         return file_count
+
+    def _salt_and_pepper(self, image):
+        """
+        Salts and Peppers image (numpy array)
+
+        :param image: numpy.array
+        :return numpy.array:
+        """
+        s_vs_p = 0.1
+        amount = 0.025
+        out = numpy.copy(image)
+        # Salt mode
+        num_salt = numpy.ceil(amount * image.size * s_vs_p)
+        coords = [numpy.random.randint(0, i - 1, int(num_salt))
+                  for i in image.shape]
+        out[tuple(coords)] = 1
+
+        # Pepper mode
+        num_pepper = numpy.ceil(amount * image.size * (1. - s_vs_p))
+        coords = [numpy.random.randint(0, i - 1, int(num_pepper))
+                  for i in image.shape]
+        out[tuple(coords)] = 0
+        return out
+
+    def _saturate(self, img, flares=False):
+        """
+        frys an image
+
+        :param img: PIL.Image
+        :param flares: bool
+        :return PIL.Image:
+        """
+        flare_positions = []
+        if flares:
+            opencv_img = cv2.cvtColor(numpy.array(img), cv2.COLOR_RGB2GRAY)
+            faces = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')\
+                .detectMultiScale(opencv_img,
+                                  scaleFactor=1.3,
+                                  minNeighbors=5,
+                                  minSize=(30, 30),
+                                  flags=cv2.CASCADE_SCALE_IMAGE
+                                  )
+            for (x, y, w, h) in faces:
+                face_roi = opencv_img[y:y+h, x:x+w]  # Get region of interest (detected face)
+                eyes = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_eye.xml').detectMultiScale(face_roi)
+                for (ex, ey, ew, eh) in eyes:
+                    eye_corner = (ex + ew / 2, ey + eh / 2)
+                    flare_size = eh if eh > ew else ew
+                    flare_size *= 4
+                    corners = [math.floor(x) for x in eye_corner]
+                    flare_positions.append(FlarePosition(*corners, flare_size))
+
+        # Crush image to hell and back
+        width, height = img.width, img.height
+        img = img.resize((int(width ** .75), int(height ** .75)), resample=Image.LANCZOS)
+        img = img.resize((int(width ** .88), int(height ** .88)), resample=Image.BILINEAR)
+        img = img.resize((int(width ** .9), int(height ** .9)), resample=Image.BICUBIC)
+        img = img.resize((width, height), resample=Image.BICUBIC)
+        img = ImageOps.posterize(img, 4)
+
+        # Generate colour overlay
+        r = img.split()[0]
+        r = ImageEnhance.Contrast(r).enhance(2.0)
+        r = ImageEnhance.Brightness(r).enhance(1.5)
+
+        r = ImageOps.colorize(r, (254, 0, 2), (255, 255, 15))
+
+        # Overlay red and yellow onto main image and sharpen the hell out of it
+        img = Image.blend(img, r, 0.75)
+        img = ImageEnhance.Sharpness(img).enhance(100.0)
+
+        # Apply flares on any detected eyes
+        if flare_positions:
+            flare_img = Image.open(STATIC_PATH + 'images/flare.png')
+            for flare in flare_positions:
+                flare_transformed = flare_img.copy().resize((flare.size,) * 2, resample=Image.BILINEAR)
+                img.paste(flare_transformed, (flare.x, flare.y), flare_transformed)
+
+        return img
+
+    def _deep_fry(self, image, flares=False):
+        file = NamedTemporaryFile(suffix='.jpg')
+        Image.fromarray(self._salt_and_pepper(numpy.asarray(self._saturate(image.convert('RGB'), flares=flares)))
+                        .astype('uint8')).save(file)
+        return file
 
     @commands.command()
     async def dankbulkupload(self, ctx, url: typing.Optional[str]):
@@ -157,3 +250,47 @@ class DankMemes(BaseCog):
                 await ctx.send(index, file=discord.File(os.path.join(MEDIA_PATH, images[index])))
             except (ValueError, RuntimeError, IndexError):
                 await ctx.send('That is not a valid meme')
+
+    @commands.command()
+    async def deepfry(self, ctx, index: typing.Optional[int]):
+        """
+        Deep frys an image
+
+        Parameters
+        ----------
+        ctx : discord.ext.commands.Context
+        index : int (optional)
+
+        Returns
+        -------
+        str
+
+        """
+        flares = True
+        # args = ctx.message.content.split(' ')[1:]
+        # if any(arg in ['-f', '--flares'] for arg in args):
+        #     flares = True
+        #     args.remove('-f')
+        if ctx.message.attachments:
+            async with ctx.message.channel.typing():
+                for attachment in ctx.message.attachments:
+                    response = requests.get(attachment.url)
+                    if response.status_code == 200:
+                        try:
+                            file = self._deep_fry(Image.open(BytesIO(response.content)), flares=flares)
+                        except OSError:
+                            await ctx.message.channel.send('I can\'t deep fry that file')
+                        else:
+                            await ctx.message.channel.send(file=discord.File(file.name))
+                    else:
+                        await ctx.message.channel.send('I could not download your file')
+        elif index is not None:
+            try:
+                if index < 0:
+                    raise IOError
+                file = self._deep_fry(Image.open(MEDIA_PATH + '{}.jpg'.format(index)), flares=flares)
+                await ctx.message.channel.send(file=discord.File(file.name))
+            except IOError:
+                await ctx.message.channel.send('That is not a valid index')
+        else:
+            await ctx.message.channel.send('There is nothing to deep fry')
