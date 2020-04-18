1a803a0a1ba31cf548d629caae4a836a5eacfb7b
Michael Hillebrand <mhillebrand@protonmail.com>
2020-04-18 12:18:17 -0700

    cleans up presence_changer.py

--- cogs/presence_changer.py
+++ cogs/presence_changer.py
@@ -7,15 +7,6 @@ from cogs.base import BaseCog
 
 
 class PresenceChanger(BaseCog):
-    """
-    from discord.enums:
-    class ActivityType(Enum):
-        unknown = -1
-        playing = 0
-        streaming = 1
-        listening = 2
-        watching = 3
-    """
 
     def __init__(self, bot) -> None:
         self.index = 0
@@ -27,19 +18,18 @@ class PresenceChanger(BaseCog):
 
     @tasks.loop(minutes=1)
     async def presence_changer(self):
-        """
-        HuskieBot will update its presence every minute with a new status from its list
+        """HuskieBot will update its presence every minute with a new status from its list
 
         Returns
         -------
             discord.Activity
         """
         try:
-            logging.info("changing presence to: type: {type}, name: {name}".format(type=self.bot.current_personality.presence_options[self.index][0],
-                                                                                   name=self.bot.current_personality.presence_options[self.index][1]))
+            presence = self.bot.current_personality.presence_options[self.index]
+            logging.info(f"changing presence to: type: {presence[0]}, name: {presence[1]}")
             await self.bot.change_presence(activity=discord.Activity(
-                type=self.bot.current_personality.presence_options[self.index][0],
-                name=self.bot.current_personality.presence_options[self.index][1]
+                type=presence[0],
+                name=presence[1]
             ))
             self.index += 1
         except IndexError:
